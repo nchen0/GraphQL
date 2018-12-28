d5c7a550b3df67089d05c7464f8c7f60fb10e0ba
Nicky Chen <nicky.chen0@gmail.com>
2018-12-28 14:29:30 -0800

    authors & posts

--- graphql-express/src/schema.js
+++ graphql-express/src/schema.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const Authors = require("./data/authors"); // This is to make available authors.json file
 const Posts = require("./data/posts"); // This is to make available post.json file
 
@@ -15,3 +16,61 @@ let {
   // This is the class we need to create the schema
   GraphQLSchema
 } = require("graphql");
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "This represent an author",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    twitterHandle: { type: GraphQLString }
+  })
+});
+
+const PostType = new GraphQLObjectType({
+  name: "Post",
+  description: "This represent a Post",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    body: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve: function(post) {
+        return Authors.find(a => a.id == post.author_id);
+      }
+    }
+  })
+});
+
+// This is the Root Query
+const RootQuery = new GraphQLObjectType({
+  name: "BlogAppSchema",
+  fields: () => ({
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: "List of all Authors",
+      resolve: function() {
+        return Authors;
+      }
+    },
+    posts: {
+      type: new GraphQLList(PostType),
+      description: "List of all Posts",
+      resolve: function() {
+        return Posts;
+      }
+    }
+  })
+});
+
+// This is the schema declaration
+const BlogSchema = new GraphQLSchema({
+  query: RootQuery
+  // If you need to create or updata a datasource,
+  // you use mutations. Note:
+  // mutations will not be explored in this post.
+  // mutation: BlogMutationRootType
+});
+
+module.exports = BlogSchema;
