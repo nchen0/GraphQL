fc2f0f4c7c8a5925d7f4c5d43d3bc0e8fe3247e6
Nicky Chen <nicky.chen0@gmail.com>
2018-12-28 16:47:41 -0800

    adding authors & posts

---
+++ graphql-yoga/src/index.js
@@ -0,0 +1,49 @@
+const { GraphQLServer } = require("graphql-yoga");
+const Authors = require("./data/authors");
+const Posts = require("./data/posts");
+
+const typeDefs = `
+    type Query {
+        info: String!
+        feed: [Link!]!
+        author: [Author!]!
+        post: [Post!]!
+    }
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
+    }
+
+    type Author {
+        id: String!
+        name: String!
+        twitterHandle: String
+    }
+
+
+`;
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    author: () => Authors
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => console.log("Server is running on port 4000"));
