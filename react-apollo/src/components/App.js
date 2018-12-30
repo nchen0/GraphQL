bf1f6631f47bfaa98959821ec1fc03b75584a636
Nicky Chen <nicky.chen0@gmail.com>
2018-12-30 12:37:58 -0800

    Querying

--- react-apollo/src/components/App.js
+++ react-apollo/src/components/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
+import LinkList from "./LinkList";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi</h1>
-      </div>
-    );
+    return <LinkList />;
   }
 }
 
